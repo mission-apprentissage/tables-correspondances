f86be3103bcdf2deb0b5c402cadae87606e77f63
Gethi <bigard.antoine@gmail.com>
2020-11-20 10:02:40 +0100

    feat: onisep url

---
+++ server/src/logic/controllers/onisep/onisepController.js
@@ -0,0 +1,40 @@
+const config = require("config");
+const { getFileFromS3 } = require("../../../common/utils/awsUtils");
+const createCatalogue = require("./onisepFileParser");
+
+class OnisepController {
+  constructor() {
+    this.catalogue = createCatalogue();
+  }
+
+  async load() {
+    console.log(config.onisep);
+    const inputStream = getFileFromS3(config.onisep);
+    await this.catalogue.loadCsvFile(inputStream);
+  }
+
+  async findUrl(cfd) {
+    const onisep_url = await this.catalogue.getUrl(cfd);
+    if (!onisep_url) {
+      return {
+        result: {
+          url: null,
+        },
+        messages: {
+          url: "Non trouvé",
+        },
+      };
+    }
+    return {
+      result: {
+        url: onisep_url,
+      },
+      messages: {
+        url: "Ok",
+      },
+    };
+  }
+}
+
+const onisepController = new OnisepController();
+module.exports = onisepController;
