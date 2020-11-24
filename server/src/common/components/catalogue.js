c7cf546c5e4038f068a148fe99c5ff1a1cd53e98
Kevin Barnoin <k.barnoin@gmail.com>
2020-11-24 18:45:16 +0100

    add catalogue component

---
+++ server/src/common/components/catalogue.js
@@ -0,0 +1,50 @@
+const axios = require("axios");
+const endpoint = "https://c7a5ujgw35.execute-api.eu-west-3.amazonaws.com/prod";
+
+module.exports = async () => {
+  return {
+    getEtablissements: async (options) => {
+      try {
+        let { page, allEtablissements, limit, query } = { page: 1, allEtablissements: [], limit: 1050, ...options };
+
+        let params = { page, limit, query };
+        logger.debug(`Requesting ${endpoint}/etablissements with parameters`, params);
+        const response = await axios.get(`${endpoint}/etablissements`, { params });
+
+        const { etablissements, pagination } = response.data;
+        allEtablissements = allEtablissements.concat(etablissements); // Should be properly exploded, function should be pure
+
+        if (page < pagination.nombre_de_page) {
+          return getEtablissements({ page: page + 1, allEtablissements, limit });
+        } else {
+          return allEtablissements;
+        }
+      } catch (error) {
+        logger.error(error);
+        return null;
+      }
+    },
+    getFormations: async (options) => {
+      try {
+        let { page, allFormations, limit } = { page: 1, allFormations: [], limit: 1050, ...options };
+
+        let params = { page, limit };
+        logger.info(`Requesting ${endpoint}/formations with parameters`, params);
+        const response = await axios.get(`${endpoint}/formations`, { params });
+
+        const { formations, pagination } = response.data;
+        allFormations = allFormations.concat(formations); // Should be properly exploded, function should be pure
+
+        if (page < pagination.nombre_de_page) {
+          // if (page < 2) {
+          return getFormations({ page: page + 1, allFormations });
+        } else {
+          return allFormations;
+        }
+      } catch (error) {
+        logger.error(error);
+        return null;
+      }
+    },
+  };
+};
--- server/src/common/components/components.js
+++ server/src/common/components/components.js
@@ -3,8 +3,10 @@ const createUsers = require("./users");
 
 module.exports = async (options = {}) => {
   const users = options.users || (await createUsers());
+  const catalogue = options.catalogue;
 
   return {
+    catalogue,
     users,
     db: options.db || (await connectToMongo()).db,
   };
