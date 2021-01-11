9361a628f8c667a122aed69e3c1c8405062665d9
Gethi <bigard.antoine@gmail.com>
2021-01-11 13:39:50 +0100

    feat: update seed user

--- server/src/jobs/seed/createUsers.js
+++ server/src/jobs/seed/createUsers.js
@@ -1,8 +1,8 @@
 const logger = require("../../common/logger");
 
 module.exports = async (users) => {
-  await users.createUser("testUser", "password");
-  await users.createUser("testAdmin", "password", { permissions: { isAdmin: true } });
-  logger.info(`User 'testUser' with password 'password' is successfully created `);
-  logger.info(`User 'testAdmin' with password 'password' and admin is successfully created `);
+  //await users.createUser("testUser", "password");
+  await users.createUser("ed", "password", { permissions: { isAdmin: true } });
+  //logger.info(`User 'testUser' with password 'password' is successfully created `);
+  logger.info(`admin is successfully created `);
 };
