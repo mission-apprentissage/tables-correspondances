2d1d5c4e9b7d8505f6bc44f53eb20136d2cc82fa
Benoît Guérout <bguerout@gmail.com>
2021-01-19 14:42:36 +0100

    Declare BCN stuff with mongo+es

--- server/src/common/model/index.js
+++ server/src/common/model/index.js
@@ -2,9 +2,9 @@ const { mongoose } = require("../mongodb");
 const { mongoosastic, getElasticInstance } = require("../esClient");
 const schema = require("../model/schema");
 
-const createModelWithES = (modelName, schema, esIndexName, createMongoDBIndexes = () => ({})) => {
+const createModelWithES = (modelName, schema, createMongoDBIndexes = () => ({})) => {
   const Schema = new mongoose.Schema(schema);
-  Schema.plugin(mongoosastic, { esClient: getElasticInstance(), index: esIndexName });
+  Schema.plugin(mongoosastic, { esClient: getElasticInstance(), index: modelName });
   Schema.plugin(require("mongoose-paginate"));
   createMongoDBIndexes(Schema);
   return mongoose.model(modelName, Schema);
@@ -17,17 +17,17 @@ const createModel = (modelName, schema) => {
 module.exports = {
   User: createModel("user", schema.userSchema),
   Log: createModel("log", schema.logSchema),
-  BcnFormationDiplome: createModel("bcnformationdiplome", schema.bcnFormationDiplomesSchema),
-  BcnLettreSpecialite: createModel("bcnlettrespecialite", schema.bcnLettreSpecialiteSchema),
-  BcnNNiveauFormationDiplome: createModel("bcnnniveauformationdiplome", schema.bcnNNiveauFormationDiplomeSchema),
-  BcnNMef: createModel("bcnnmef", schema.bcnNMefSchema),
-  BcnNDispositifFormation: createModel("bcnndispositifformation", schema.bcnNDispositifFormationSchema),
+  BcnFormationDiplome: createModelWithES("bcnformationdiplome", schema.bcnFormationDiplomesSchema),
+  BcnLettreSpecialite: createModelWithES("bcnlettrespecialite", schema.bcnLettreSpecialiteSchema),
+  BcnNNiveauFormationDiplome: createModelWithES("bcnnniveauformationdiplome", schema.bcnNNiveauFormationDiplomeSchema),
+  BcnNMef: createModelWithES("bcnnmef", schema.bcnNMefSchema),
+  BcnNDispositifFormation: createModelWithES("bcnndispositifformation", schema.bcnNDispositifFormationSchema),
   FicheRncp: createModel("ficherncp", schema.ficheRncpSchema),
   ConventionFile: createModel("conventionfile", schema.conventionFileSchema),
   CodeIdccOpco: createModel("codeIdccOpco", schema.codeIdccOpcoSchema),
   CodeEnCodesIdcc: createModel("codeEnCodesIdcc", schema.codeEnCodesIdccSchema),
-  DomainesMetiers: createModelWithES("domainesmetiers", schema.domainesMetiersSchema, "domainesmetiers"),
-  Etablissement: createModelWithES("etablissement", schema.etablissementSchema, "etablissements", (schema) => {
+  DomainesMetiers: createModelWithES("domainesmetiers", schema.domainesMetiersSchema),
+  Etablissement: createModelWithES("etablissement", schema.etablissementSchema, (schema) => {
     schema.index({ adresse: "text" });
   }),
 };
