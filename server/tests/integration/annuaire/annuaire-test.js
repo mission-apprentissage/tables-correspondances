6fbc51f01736bdf078eec490c5dc8f6bf1c739cc
Benoît Guérout <bguerout@gmail.com>
2021-01-19 17:28:58 +0100

    Rename dummy

--- server/tests/integration/annuaire/annuaire-test.js
+++ server/tests/integration/annuaire/annuaire-test.js
@@ -16,7 +16,7 @@ integrationTests(__filename, () => {
     );
   };
 
-  const createDummyParser = () => {
+  const createTestParser = () => {
     //Works with a fake CSV file with the following columns
     //uai;siret;nom
     return oleoduc(
@@ -92,13 +92,13 @@ integrationTests(__filename, () => {
   });
 
   it("Vérifie qu'on ajoute un uai quand il n'existe pas", async () => {
-    let source = createStream(
+    let stream = createStream(
       `uai;siret;nom
 "0011073L";"11111111111111";"Centre de formation"`
     );
 
     await annuaire.initialize(createDEPPStream());
-    let results = await annuaire.collect("dummy", source, { parser: createDummyParser() });
+    let results = await annuaire.collect("test", stream, { parser: createTestParser() });
 
     let found = await Annuaire.findOne();
     assert.deepStrictEqual(omit(found.toObject(), ["__v", "_id"]), {
@@ -112,7 +112,7 @@ integrationTests(__filename, () => {
           valid: true,
         },
         {
-          type: "dummy",
+          type: "test",
           uai: "0011073L",
           valid: true,
         },
@@ -126,17 +126,17 @@ integrationTests(__filename, () => {
   });
 
   it("Vérifie qu'on teste la validaté d'un UAI", async () => {
-    let source = createStream(
+    let stream = createStream(
       `uai;siret;nom
 "093XXXT";"11111111111111";"Centre de formation"`
     );
 
     await annuaire.initialize(createDEPPStream());
-    let results = await annuaire.collect("dummy", source, { parser: createDummyParser() });
+    let results = await annuaire.collect("test", stream, { parser: createTestParser() });
 
     let found = await Annuaire.findOne();
     assert.deepStrictEqual(found.toObject().uais[1], {
-      type: "dummy",
+      type: "test",
       uai: "093XXXT",
       valid: false,
     });
@@ -148,13 +148,13 @@ integrationTests(__filename, () => {
   });
 
   it("Vérifie qu'on ignore un uai quand il existe en tant qu'uai principal", async () => {
-    let source = createStream(
+    let stream = createStream(
       `uai;siret;nom
 "0011058V";"11111111111111";"Centre de formation"`
     );
 
     await annuaire.initialize(createDEPPStream());
-    let stats = await annuaire.collect("dummy", source, { parser: createDummyParser() });
+    let stats = await annuaire.collect("test", stream, { parser: createTestParser() });
 
     let found = await Annuaire.findOne();
     assert.deepStrictEqual(omit(found.toObject(), ["__v", "_id"]), {
@@ -177,7 +177,7 @@ integrationTests(__filename, () => {
   });
 
   it("Vérifie qu'on ignore un uai quand il existe déjà en tant qu'uai secondaire", async () => {
-    let source = createStream(
+    let stream = createStream(
       `uai;siret;nom
 "0011073L";"11111111111111";"Centre de formation"`
     );
@@ -192,14 +192,14 @@ integrationTests(__filename, () => {
           valid: true,
         },
         {
-          type: "dummy",
+          type: "test",
           uai: "0011073L",
           valid: true,
         },
       ],
     }).save();
 
-    let stats = await annuaire.collect("dummy", source, { parser: createDummyParser() });
+    let stats = await annuaire.collect("test", stream, { parser: createTestParser() });
 
     let found = await Annuaire.findOne();
     assert.deepStrictEqual(omit(found.toObject(), ["__v", "_id"]), {
@@ -213,7 +213,7 @@ integrationTests(__filename, () => {
           valid: true,
         },
         {
-          type: "dummy",
+          type: "test",
           uai: "0011073L",
           valid: true,
         },
@@ -227,13 +227,13 @@ integrationTests(__filename, () => {
   });
 
   it("Vérifie qu'on ignore un uai vide", async () => {
-    let source = createStream(
+    let stream = createStream(
       `uai;siret;nom
 "";"11111111111111";"Centre de formation"`
     );
 
     await annuaire.initialize(createDEPPStream());
-    let stats = await annuaire.collect("dummy", source, { parser: createDummyParser() });
+    let stats = await annuaire.collect("test", stream, { parser: createTestParser() });
 
     let found = await Annuaire.findOne();
     assert.deepStrictEqual(omit(found.toObject(), ["__v", "_id"]), {
@@ -256,13 +256,13 @@ integrationTests(__filename, () => {
   });
 
   it("Vérifie qu'on peut collecter des informations du fichier ONISEP", async () => {
-    let source = createStream(
+    let stream = createStream(
       `"code UAI";"n° SIRET";"nom"
 "0011073L";"11111111111111";"Centre de formation"`
     );
 
     await annuaire.initialize(createDEPPStream());
-    let results = await annuaire.collect("onisep", source);
+    let results = await annuaire.collect("onisep", stream);
 
     let found = await Annuaire.findOne();
     assert.deepStrictEqual(omit(found.toObject(), ["__v", "_id"]), {
@@ -290,13 +290,13 @@ integrationTests(__filename, () => {
   });
 
   it("Vérifie qu'on peut collecter des informations du fichier REFEA", async () => {
-    let source = createStream(
+    let stream = createStream(
       `uai_code_siret;uai_code_educnationale;uai_libelle_educnationale
 "11111111111111";"0011073L";"Centre de formation"`
     );
 
     await annuaire.initialize(createDEPPStream());
-    let results = await annuaire.collect("refea", source);
+    let results = await annuaire.collect("refea", stream);
 
     let found = await Annuaire.findOne();
     assert.deepStrictEqual(omit(found.toObject(), ["__v", "_id"]), {
