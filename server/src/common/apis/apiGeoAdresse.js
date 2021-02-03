4384551c9bbbf6699ebf3eb702d114f92dd6fb97
Benoît Guérout <bguerout@gmail.com>
2021-02-03 14:11:14 +0100

    Make api geo adresse reusable

--- server/src/common/apis/apiGeoAdresse.js
+++ server/src/common/apis/apiGeoAdresse.js
@@ -1,4 +1,6 @@
 const axios = require("axios");
+const queryString = require("query-string");
+const logger = require("../logger");
 
 // Cf Documentation : https://geo.api.gouv.fr/adresse
 const apiEndpoint = "https://api-adresse.data.gouv.fr";
@@ -6,36 +8,25 @@ const apiEndpoint = "https://api-adresse.data.gouv.fr";
 class ApiGeoAdresse {
   constructor() {}
 
-  async search(q, postcode = null) {
-    try {
-      const response = await axios.get(`${apiEndpoint}/search/?q=${q}${postcode ? `&postcode=${postcode}` : ""}`);
-      return response.data;
-    } catch (error) {
-      console.error(`geo search error : ${q} ${postcode} ${error}`);
-      return null;
-    }
+  async search(q, options = {}) {
+    let params = queryString.stringify({ q, ...options });
+    logger.debug(`[Adresse API] Searching adresse with parameters ${params}...`);
+    const response = await axios.get(`${apiEndpoint}/search/?${params}`);
+    return response.data;
   }
 
-  async searchPostcodeOnly(q, postcode = null) {
-    try {
-      const response = await axios.get(`${apiEndpoint}/search/?q=${q}${postcode ? `&postcode=${postcode}` : ""}`);
-      return response.data;
-    } catch (error) {
-      console.error(`geo searchPostcodeOnly error : #${q}# ${postcode} ${error}`);
-      return null;
-    }
+  async searchPostcodeOnly(q, options = {}) {
+    let params = queryString.stringify({ q, ...options });
+    const response = await axios.get(`${apiEndpoint}/search/?${params}`);
+    logger.debug(`[Adresse API] Searching Postcode with parameters ${params}...`);
+    return response.data;
   }
 
-  async searchMunicipalityByCode(code, isCityCode = false) {
-    try {
-      const { data } = await axios.get(
-        `${apiEndpoint}/search/?limit=1&q=${isCityCode ? "citycode=" : ""}${code}&type=municipality`
-      );
-      return data;
-    } catch (e) {
-      console.error("geo search municipality error", e);
-      return e;
-    }
+  async searchMunicipalityByCode(code, options = {}) {
+    let params = `${options.isCityCode ? "citycode=" : ""}${code}&type=municipality`;
+    logger.debug(`[Adresse API] Searching municipality with parameters ${params}...`);
+    const response = await axios.get(`${apiEndpoint}/search/?limit=1&q=${params}`);
+    return response.data;
   }
 }
 
