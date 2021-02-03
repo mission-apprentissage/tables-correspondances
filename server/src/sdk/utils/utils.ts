ba41055cb533738f18f3eb6a339d4b8763cb87df
Gethi <bigard.antoine@gmail.com>
2021-02-03 18:23:35 +0100

    feat: try catch

--- server/src/sdk/utils/utils.ts
+++ server/src/sdk/utils/utils.ts
@@ -3,6 +3,11 @@ import { getDataFromCP } from "../../logic/handlers/geoHandler";
 export async function getCpInfo(
   codePostal: string,
 ): Promise<any> {
-  const result = await getDataFromCP(codePostal);
-  return result;
+  try {
+    const result = await getDataFromCP(codePostal);
+    return result;
+  } catch (error) {
+    console.error(`getCpInfo: something went wrong`);
+    return null;
+  }
 }
