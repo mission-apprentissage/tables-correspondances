3b8facd4f3b3915dc541cebdd4af03fd1c9e5856
Gethi <bigard.antoine@gmail.com>
2021-02-03 18:05:43 +0100

    feat: move inside server folder

---
+++ server/src/sdk/importers/importer.ts
@@ -0,0 +1,9 @@
+// import { getDataFromCP } from "../../logic/handlers/geoHandler";
+
+export async function dostuff(
+  codePostal: string,
+): Promise<any> {
+  // const result = await getDataFromCP(codePostal);
+  console.log(codePostal);
+  return "dddd";
+}
