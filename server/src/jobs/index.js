0a9db7492c692d42c4b346be3a19807b86f434da
Benoît Guérout <bguerout@gmail.com>
2021-02-04 11:38:30 +0100

    Ajout de rncpImporter et EtablissementsUpdater dans jobs

--- server/src/jobs/index.js
+++ server/src/jobs/index.js
@@ -2,12 +2,16 @@ const { runScript } = require("./scriptWrapper");
 const logger = require("../common/logger");
 const downloadBcnTables = require("./bcnDownloader/index");
 const importBcnTables = require("./bcnImporter/index");
+const rncpImporter = require("./rncpImporter/index");
+const EtablissementsUpdater = require("./EtablissementsUpdater/index");
 
 runScript(async ({ db }) => {
   try {
     logger.info(`Start all jobs`);
     await downloadBcnTables();
     await importBcnTables(db);
+    await rncpImporter();
+    await EtablissementsUpdater();
   } catch (error) {
     logger.error(error);
   }
