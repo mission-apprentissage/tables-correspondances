b56af7d20ffc80f2b5351b97a85d450ec9c58803
Benoît Guérout <bguerout@gmail.com>
2021-04-21 17:31:08 +0200

    Add Reseaux into annuaire export

--- server/src/jobs/annuaire/exports.js
+++ server/src/jobs/annuaire/exports.js
@@ -14,6 +14,7 @@ module.exports = {
             Statut: (a) => a.statut,
             "Code Postal": (a) => a.adresse.code_postal,
             Ville: (a) => a.adresse.localite,
+            Reseaux: (a) => a.reseaux.join("|"),
             UAIs: (a) => a.uais.map(({ uai }) => uai).join("|"),
             Sources: (a) => a.uais.flatMap(({ sources }) => sources).join("|"),
           },
