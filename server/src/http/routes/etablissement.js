ac40569f51966fe4224bce155226267ba55ec604
Eric Plaquevent <eric.plaquevent@gmail.com>
2021-10-27 15:10:23 +0200

    fix: proxy etablissement routes to catalogue

--- server/src/http/routes/etablissement.js
+++ server/src/http/routes/etablissement.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const axios = require("axios");
 const tryCatch = require("../middlewares/tryCatchMiddleware");
+const { sendJsonStream } = require("../../common/utils/httpUtils");
 
 const CATALOGUE_API_URL =
   process.env.TABLES_CORRESPONDANCES_ENV === "production"
@@ -15,43 +16,49 @@ module.exports = () => {
 
   router.get(
     "/etablissements",
-    tryCatch(async (req) => {
-      return await axios.get(`${CATALOGUE_API_URL}/entity/etablissements`, { params: req.query });
+    tryCatch(async (req, res) => {
+      const stream = await axios.get(`${CATALOGUE_API_URL}/entity/etablissements`, { params: req.query });
+      return sendJsonStream(stream, res);
     })
   );
 
   router.get(
     "/etablissements.ndjson",
-    tryCatch(async (req) => {
-      return await axios.get(`${CATALOGUE_API_URL}/entity/etablissements.ndjson`, { params: req.query });
+    tryCatch(async (req, res) => {
+      const stream = await axios.get(`${CATALOGUE_API_URL}/entity/etablissements.ndjson`, { params: req.query });
+      return sendJsonStream(stream, res);
     })
   );
 
   router.get(
     "/etablissements/count",
-    tryCatch(async (req) => {
-      return await axios.get(`${CATALOGUE_API_URL}/entity/etablissements/count`, { params: req.query });
+    tryCatch(async (req, res) => {
+      const data = await axios.get(`${CATALOGUE_API_URL}/entity/etablissements/count`, { params: req.query });
+      return res.json(data);
     })
   );
 
   router.get(
     "/etablissement",
-    tryCatch(async (req) => {
-      return await axios.get(`${CATALOGUE_API_URL}/entity/etablissement`, { params: req.query });
+    tryCatch(async (req, res) => {
+      const data = await axios.get(`${CATALOGUE_API_URL}/entity/etablissement`, { params: req.query });
+      return res.json(data);
     })
   );
 
   router.get(
     "/etablissement/:id",
-    tryCatch(async (req) => {
-      return await axios.get(`${CATALOGUE_API_URL}/entity/etablissement/${req.params.id}`);
+    tryCatch(async (req, res) => {
+      const data = await axios.get(`${CATALOGUE_API_URL}/entity/etablissement/${req.params.id}`);
+      return res.json(data);
     })
   );
 
   router.get(
     "/etablissements/siret-uai",
-    tryCatch(async (req) => {
-      return await axios.get(`${CATALOGUE_API_URL}/entity/etablissements/siret-uai`, { params: req.query });
+    tryCatch(async (req, res) => {
+      const data = await axios.get(`${CATALOGUE_API_URL}/entity/etablissements/siret-uai`, { params: req.query });
+      return res.json(data);
     })
   );
 
