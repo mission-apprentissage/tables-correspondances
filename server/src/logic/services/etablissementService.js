796037f24167ce8e5627ad47a4f65100236c86dc
Eric Plaquevent <eric.plaquevent@gmail.com>
2021-11-02 09:53:45 +0100

    perf(etablissement): don't retrieve geoloc if we already have it
    since address never changes for a given siret

--- server/src/logic/services/etablissementService.js
+++ server/src/logic/services/etablissementService.js
@@ -59,14 +59,24 @@ const etablissementService = async (
       numero_voie: etablissement.numero_voie,
       type_voie: etablissement.type_voie,
       nom_voie: etablissement.nom_voie,
+      geo_coordonnees: etablissement.geo_coordonnees,
     };
 
     let updatedEtablissement = {};
 
     // ENTREPRISE DATA
     if (scope.siret) {
-      // console.log("Update siret info");
-      const { result: siretMapping, messages: siretMessages } = await getDataFromSiret(etablissement.siret);
+      let withGeoloc = true;
+
+      // don't retrieve geoloc if we already have it, since address never changes for a given siret
+      if (current.geo_coordonnees) {
+        withGeoloc = false;
+        updatedEtablissement = current.geo_coordonnees;
+      }
+
+      const { result: siretMapping, messages: siretMessages } = await getDataFromSiret(etablissement.siret, {
+        withGeoloc,
+      });
 
       let error = parseErrors(siretMessages);
       if (error) {
@@ -97,7 +107,6 @@ const etablissementService = async (
     if (scope.geoloc && !scope.siret) {
       // check scope.siret because geoloc is already retrieved by getDataFromSiret
 
-      // console.log("Update geoloc info");
       const { result: geoMapping, messages: geoMessages } = await getCoordinatesFromAddressData({
         numero_voie: current.numero_voie,
         type_voie: current.type_voie,
@@ -119,7 +128,6 @@ const etablissementService = async (
 
     // CONVENTIONNEMENNT DATA
     if (scope.conventionnement) {
-      // console.log("Update conventionnement info");
       const conventionData = await conventionController.getConventionData(
         current.siret,
         etablissement.uai,
