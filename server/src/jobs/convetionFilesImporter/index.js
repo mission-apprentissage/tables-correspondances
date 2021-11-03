4f929ad0ea8ad556c5d1ddd266597159b4f744a0
Eric Plaquevent <eric.plaquevent@gmail.com>
2021-11-03 11:32:47 +0100

    feat: add convention importer in sdk

--- server/src/jobs/convetionFilesImporter/index.js
+++ server/src/jobs/convetionFilesImporter/index.js
@@ -7,8 +7,8 @@ const config = require("config");
 const path = require("path");
 const XLSX = require("xlsx");
 
-const downloadXlsxAndGetJson = async (filename, download = true) => {
-  const local_path = path.join(__dirname, `./assets/${filename}`);
+const downloadXlsxAndGetJson = async (assetsDir, filename, download = true) => {
+  const local_path = path.join(assetsDir, filename);
   if (download) {
     await downloadAndSaveFileFromS3(`${config.conventionFiles.path}/${filename}`, local_path);
   }
@@ -16,7 +16,7 @@ const downloadXlsxAndGetJson = async (filename, download = true) => {
   return XLSX.utils.sheet_to_json(datadockWb.workbook.Sheets[datadockWb.sheet_name_list[0]]);
 };
 
-const conventionFilesImporter = async (db) => {
+const conventionFilesImporter = async (db, assetsDir = path.join(__dirname, "./assets")) => {
   logger.warn(`[Convention files importer] Starting`);
 
   logger.info(`[Convention files importer] removing conventionfiles documents`);
@@ -24,18 +24,18 @@ const conventionFilesImporter = async (db) => {
   logger.info(`[Convention files importer] Removing successfull`);
 
   // CSV import
-  const PUBLIC_OFS_PATH = path.join(__dirname, "./assets/latest_public_ofs.csv");
+  const PUBLIC_OFS_PATH = path.join(assetsDir, "latest_public_ofs.csv");
   await downloadFile("https://www.data.gouv.fr/fr/datasets/r/745a5413-d2b5-4d61-b743-8b0ace68083b", PUBLIC_OFS_PATH); // latest_public_ofs.csv
   const publicOfs = getJsonFromCsvFile(PUBLIC_OFS_PATH);
 
-  const DEPP_PATH = path.join(__dirname, "./assets/CFASousConvRegionale_latest-UAI.csv");
+  const DEPP_PATH = path.join(assetsDir, "CFASousConvRegionale_latest-UAI.csv");
   const depp = getJsonFromCsvFile(DEPP_PATH);
 
-  const Datadock_PATH = path.join(__dirname, "./assets/BaseDataDock-latest.csv");
+  const Datadock_PATH = path.join(assetsDir, "BaseDataDock-latest.csv");
   const datadock = getJsonFromCsvFile(Datadock_PATH, ",");
 
   // Xlsx import
-  const dgefp = await downloadXlsxAndGetJson("DGEFP - Extraction au 10 01 2020.xlsx");
+  const dgefp = await downloadXlsxAndGetJson(assetsDir, "DGEFP - Extraction au 10 01 2020.xlsx");
 
   // Push into Db
   await importConventionFiles(db, publicOfs, datadock, depp, dgefp);
