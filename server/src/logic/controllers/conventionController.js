de6880ba992e4378fd3f5d14f51179d3c9ec204c
Quentin Petel <QuentinPetel@users.noreply.github.com>
2022-02-09 10:22:02 +0100

    Clean / Remove DEPP and DGEFP convention files (#201)
    * Remove DEPP and DGEFP convention files
    
    * Remove unused code, add tests for miscUtils

--- server/src/logic/controllers/conventionController.js
+++ server/src/logic/controllers/conventionController.js
@@ -1,17 +1,6 @@
 const { ConventionFile } = require("../../common/model/index");
 
 const infosCodes = {
-  infoDEPP: {
-    MissingUai: -1,
-    NotFound: 0,
-    Found: 1,
-  },
-  infoDGEFP: {
-    NotFound: 0,
-    SirenMatch: 1,
-    SiretMatch: 2,
-    SiretSiegeSocialMatch: 3,
-  },
   infoDATAGOUV: {
     NotFound: 0,
     Found: 1,
@@ -24,18 +13,12 @@ class ConventionController {
     const data = this.validateFormat(providedSiret, providedUai, providedSiretSiegeSocial);
     if (data.messages.error) return data;
 
-    const { siret, uai, siretSiegeSocial } = data.result;
+    const { siret } = data.result;
 
-    const info_depp = await this.findInfoDepp(uai);
-    const info_dgefp = await this.findInfoDgefp(siret, siretSiegeSocial);
     const info_datagouv_ofs = await this.findInfoDataGouv(siret);
 
     return {
-      info_depp: info_depp.value,
-      info_dgefp: info_dgefp.value,
       info_datagouv_ofs: info_datagouv_ofs.value,
-      info_depp_info: info_depp.info,
-      info_dgefp_info: info_dgefp.info,
       info_qualiopi_info: info_datagouv_ofs.qualiopi ? "OUI" : "NON",
       info_datagouv_ofs_info: info_datagouv_ofs.info,
       nda: info_datagouv_ofs.data?.numerodeclarationactivite || null,
@@ -88,45 +71,6 @@ class ConventionController {
         uai: "Ok",
       },
     };
-  }
-
-  async findInfoDepp(uai) {
-    const result = await ConventionFile.findOne({ type: "DEPP", numero_uai: uai });
-    if (!result) {
-      return { info: "Erreur: Depp Non trouvé", value: infosCodes.infoDEPP.NotFound };
-    }
-    return { info: "Ok", value: infosCodes.infoDEPP.Found };
-  }
-
-  async findInfoDgefp(siret, siretSiegeSocial) {
-    const siren = siret.substring(0, 9);
-    const numEtablissment = siret.substring(9, 14);
-    const resultSiren = await ConventionFile.findOne({ type: "DGEFP", "N° SIREN": siren });
-    if (!resultSiren) {
-      const resultSiret = await ConventionFile.findOne({
-        type: "DGEFP",
-        "N° SIREN": siren,
-        "N° Etablissement": numEtablissment,
-      });
-
-      if (!resultSiret) {
-        const sirenSiege = siretSiegeSocial.substring(0, 9);
-        const numEtablissmentSiege = siretSiegeSocial.substring(9, 14);
-        const resultSiretSiege = await ConventionFile.findOne({
-          type: "DGEFP",
-          "N° SIREN": sirenSiege,
-          "N° Etablissement": numEtablissmentSiege,
-        });
-
-        if (!resultSiretSiege) {
-          return { info: "Erreur: Dgefp Non trouvé", value: infosCodes.infoDGEFP.NotFound };
-        }
-
-        return { info: "Ok siret siege", value: infosCodes.infoDGEFP.SiretSiegeSocialMatch };
-      }
-      return { info: "Ok siret", value: infosCodes.infoDGEFP.SiretMatch };
-    }
-    return { info: "Ok siren", value: infosCodes.infoDGEFP.SirenMatch };
   }
 
   async findInfoDataGouv(siret) {
