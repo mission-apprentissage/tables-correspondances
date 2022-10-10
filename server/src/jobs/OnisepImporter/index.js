6013905fc350d95d6b4a182e0afa42100465507b
Quentin Petel <contact@quentinpetel.fr>
2022-10-10 14:00:11 +0200

    fix: Onisep importer

--- server/src/jobs/OnisepImporter/index.js
+++ server/src/jobs/OnisepImporter/index.js
@@ -1,19 +1,20 @@
 const logger = require("../../common/logger");
 const { runScript } = require("../scriptWrapper");
 const apiOnisep = require("../../common/apis/apiOnisep");
+const { Onisep } = require("../../common/model");
 
-const onisepImporter = async (db) => {
+const onisepImporter = async () => {
   logger.info(`[Onisep importer] Starting`);
 
   logger.info(`[Onisep importer] removing onisep documents`);
-  await db.collection("oniseps").deleteMany({});
+  await Onisep.deleteMany({});
   logger.info(`[Onisep importer] Removing successfull`);
 
   const etablissements = await apiOnisep.getAllEtablissements();
   const formations = await apiOnisep.getAllFormations();
 
-  await db.collection("oniseps").insertMany(etablissements.map((d) => ({ ...d, type: "etablissement" })));
-  await db.collection("oniseps").insertMany(formations.map((d) => ({ ...d, type: "formation" })));
+  await Onisep.insertMany(etablissements.map((d) => ({ ...d, type: "etablissement" })));
+  await Onisep.insertMany(formations.map((d) => ({ ...d, type: "formation" })));
 
   logger.info(`[Onisep importer] Ended`);
 };
