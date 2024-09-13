c566567ef8165bb708196f40c3e260b82d3d2e80
Quentin Petel <contact@quentinpetel.fr>
2024-09-13 15:26:58 +0200

    fix: Mise à jour des dpendances pour résoudre des vulnérabilités critiques

--- server/src/common/model/swaggerSchema/onisep.js
+++ server/src/common/model/swaggerSchema/onisep.js
@@ -6,6 +6,37 @@ module.exports = {
         type: "string",
         description: "Type (etablissement | formation)",
       },
+      code_uai: {
+        type: "string",
+        description: "uai",
+      },
+      academie: {
+        type: "string",
+        description: "academie",
+      },
+      code_mef: {
+        type: "string",
+        description: "Code Mef",
+      },
+      code_formation_diplome: {
+        type: "string",
+        description: "Code formation diplome (EN)",
+      },
+      libelle_formation_principal: {
+        type: "string",
+      },
+      libelle_poursuite: {
+        type: "string",
+      },
+      lien_site_onisepfr: {
+        type: "string",
+      },
+      discipline: {
+        type: "string",
+      },
+      domaine_sousdomaine: {
+        type: "string",
+      },
       _id: {
         type: "string",
         pattern: "^[0-9a-fA-F]{24}$",
